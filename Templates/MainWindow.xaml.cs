894a538151c26c3984235024c5e0fc18d049224b
KlaasHagge <klaash@ppedv.de>
2020-11-04 15:15:46 +0100

    Templates

---
+++ Templates/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Templates
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<Person> Personenliste { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Personenliste = new ObservableCollection<Person>()
+            {
+                new Person(){Vorname="Rainer", Nachname="Zufall", Alter=45},
+                new Person(){Vorname="Anna", Nachname="Nass", Alter=23},
+            };
+
+            this.DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Klick funktioniert");
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            (Spl_DataBinding.DataContext as Person).Alter++;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Personenliste.Add(new Person() { Vorname = "Hannes", Nachname = "Schmidt", Alter = 101 });
+        }
+    }
+}
---
+++ Templates/Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Templates
+{
+    public class Person : INotifyPropertyChanged
+    {
+        public string Vorname { get; set; }
+        public string Nachname { get; set; }
+
+        private int alter;
+        public int Alter
+        {
+            get { return alter; }
+            set 
+            { 
+                alter = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Alter)));
+            }
+        }
+
+        public override string ToString()
+        {
+            return this.Vorname + " " + this.Nachname;
+        } 
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
