0f1687e365e338b1d5356badc9f3472a6ae22c64
KlaasHagge <klaash@ppedv.de>
2020-11-04 11:33:34 +0100

    Revert "Revert "Resources, Bindings, ValueConverter""
    This reverts commit 408306c0c9ce246b611fb3fdee1dbddd58fef22c.

---
+++ RoutedEvents/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SP_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Tbl_Output.Text += (sender as StackPanel).Name + " Tunnel/Preview\n";
+        }
+
+        private void SP_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Tbl_Output.Text += (sender as StackPanel).Name + " Bubble\n";
+
+            if ((sender as StackPanel).Name == "Gelb")
+                e.Handled = true;
+        }
+    }
+}
