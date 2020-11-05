7897fae3c21df54856fd8a795df1c46b4502def7
KlaasHagge <klaash@ppedv.de>
2020-11-05 11:48:02 +0100

    Validation (kommentiert)

--- Personendatenbank/Person.cs
+++ Personendatenbank/Person.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Text;
 using System.Windows.Media;
 
@@ -7,7 +9,7 @@ namespace Personendatenbank
 {
     public enum Gender { Männlich, Weiblich, Divers }
 
-    public class Person
+    public class Person : IDataErrorInfo
     {
         public string Vorname { get; set; }
 
@@ -20,5 +22,46 @@ namespace Personendatenbank
         public Color Lieblingsfarbe { get; set; }
 
         public Gender Geschlecht { get; set; }
+
+        public Person()
+        {
+            this.Vorname = "";
+            this.Nachname = "";
+            this.Geburtsdatum = DateTime.Now.AddDays(1);
+        }
+
+        public string Error
+        {
+            get { return ""; }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case nameof(Vorname):
+                        if (Vorname.Length <= 0 || Vorname.Length > 50) return "Bitte geben Sie Ihren Vornamen ein.";
+                        if (!Vorname.All(x => Char.IsLetter(x))) return "Der Vorname darf nur Buchstaben einthalten.";
+                        break;
+
+                    case nameof(Nachname):
+                        if (Nachname.Length <= 0 || Nachname.Length > 50) return "Bitte geben Sie Ihren Nachnamen ein.";
+                        if (!Nachname.All(x => Char.IsLetter(x))) return "Der Nachname darf nur Buchstaben einthalten.";
+                        break;
+
+                    case nameof(Geburtsdatum):
+                        if (Geburtsdatum > DateTime.Now) return "Das Geburtsdatum darf nicht in der Zukunft liegen.";
+                        if (DateTime.Now.Year - Geburtsdatum.Year > 150) return "Das Geburtsdatum darf nicht mehr als 150 Jahre in der Vergangenheit liegen.";
+                        break;
+
+                    case nameof(Lieblingsfarbe):
+                        if (Lieblingsfarbe.ToString().Equals("#00000000")) return "Wählen Sie Ihre Lieblingsfarbe aus.";
+                        break;
+                }
+                return "";
+            }
+        }
     }
 }
---
+++ Validation/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Validation
+{
+    //Für ValidatesOnDataErrors muss z.B. das Interface IDataErrorInfo implementiert werden. Dieses erfordert die Einbindung von zwei zusätzlichen
+    public class Person : IDataErrorInfo
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value.All(x => Char.IsLetter(x)))
+                    name = value;
+
+                else
+                    //Bei einer Validierung per Exceptions wird die Exception-Message als Fehlemeldung verwendet. Die Exception wird automatisch von
+                    //der GUI abgefangen (wenn in der Bindung ValidatesOnExceptions true ist)
+                    throw new Exception("Bitte gib nur Buchstaben ein.");
+            }
+        }
+
+        public int Alter { get; set; }
+
+        public string Error
+        {
+            get { return ""; }
+        }
+
+        //Diese Property wird von dem Interface gefordert und von der GUI als Validierung verwendet. Wenn ein nicht-leerer String zurückgegeben wird, 
+        //dann wird dies als Fehler interpretiert und dieser String als Fehlermeldung
+        public string this[string columnName]
+        {
+            get
+            {
+                switch (columnName)
+                {
+                    case (nameof(Alter)):
+                        if (Alter < 0 | Alter > 150) return "Bitte gib dein wahres Alter an.";
+
+                        break;
+                    default:
+                        break;
+                }
+                return "";
+            }
+        }
+    }
+}
